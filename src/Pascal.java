cb9bb504a88a5e9859b38ab5e95037ec44f288a6
jimender2 <jmeredit@ashland.edu>
2018-06-03 22:25:42 -0400

    update

---
+++ src/Pascal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pascal {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int rowNumber,
+			userRowNumber,
+			i=0;
+		String lastRow,
+			currentRow;
+			
+		System.out.println("Enter the row number you want to calculate to");
+		userRowNumber = scan.nextInt();
+		
+		while(i <= userRowNumber) {
+			
+		}
+	}
+}
