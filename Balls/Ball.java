9311456e0bbd62a76554a0bf107606be36ba0036
jimender2 <jmeredit@ashland.edu>
2018-06-08 22:35:23 -0400

    updated ball class

--- Balls/Ball.java
+++ Balls/Ball.java
@@ -11,11 +11,17 @@ public class Ball {
 	private int ballSpeedX;
 	private int ballSpeedY;
 	
+	
+	//   private float ballSpeedX = rand.nextInt(10);   // Ball's speed for x and y
+	//   private float ballSpeedY = rand.nextInt(10);
+	
 	public ball() {
 		Random rand = new Random();
 		ballRadius = rand.nextInt(25);
 		ballX = ballRadius + rand.nextInt(100);
 		ballY = ballRadius + rand.nextInt(100);
+		ballSpeedX = rand.nextInt(10);
+		ballSpeedY = rand.nextInt(10);
 	}
 	
 	public ball(int radius) {
@@ -23,6 +29,8 @@ public class Ball {
 		ballRadius = radius;
 		ballX = ballRadius + rand.nextInt(100);
 		ballY = ballRadius + rand.nextInt(100);
+		ballSpeedX = rand.nextInt(10);
+		ballSpeedY = rand.nextInt(10);
 	}
 	
 	public ball(int radius, int x, int y) {
@@ -30,5 +38,21 @@ public class Ball {
 		ballRadius = radius;
 		ballX = ballRadius + x;
 		ballY = ballRadius + y;
+	}
+	
+	public void setBallX(int x) {
+		ballX = x;
+	}
+	
+	public void setBallY(int y) {
+		ballY = y;
+	}
+	
+	public void setBallSpeedX(int x) {
+		ballSpeedX = x;
+	}
+	
+	public void setBallSpeedY(int y) {
+		ballSpeedY = y;
 	}
 }
